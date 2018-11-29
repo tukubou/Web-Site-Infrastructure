c8139f5f95cc8a84fa439c6eafbb4f4edc2f0a4e
Yuta-Shimogishi <tukubou417@gmail.com>
2018-11-29 20:38:21 +0900

    ヘッダー100% &　色がつけられるようになった

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laravel</title>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    </head>
-    <body>
-        <div id="app">
-          <div class="container">
-            <vue-header></vue-header>
-            <router-view></router-view>
-          </div>
-        </div>
-    </body>
-    <script src="{{ mix('js/app.js') }}"></script>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Laravel</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+  <div id="app" class="container-fluid p-0">
+      <!-- 共通 Header -->
+      <vue-header></vue-header>
+      <!-- ルートごとに変わるコンテンツ -->
+      <router-view></router-view>
+  </div>
+</body>
+<script src="{{ mix('js/app.js') }}"></script>
+
 </html>
